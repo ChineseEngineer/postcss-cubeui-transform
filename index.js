86242d0cf3837686295006f86385a0ca7a39aadb
FE-TianYongGang <tianyonggang@aibao.com>
2019-04-09 13:45:25 +0800

    新增忽略目录功能

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var postcss = require('postcss')
 
-function transform (opts, decl) {
-  var reg = /\d+px/gi
-  decl.value = decl.value.replace(reg, function (val) {
-    val = val.toLowerCase()
-    if (val === '1px') return val
-    return parseFloat(val) * 2 + 'px'
-  })
-}
-
 module.exports = postcss.plugin('postcss-cubeui-transform', function (opts) {
   opts = opts || {}
 
   // Work with options here
-  return css => css.walkDecls(transform.bind(null, opts))
+  return function (css) {
+    css.walkDecls(function (decl) {
+      if (opts.exclude && css.source.input.file && css.source.input.file.match(opts.exclude) !== null) return;
+      var reg = /\d+px/gi
+      decl.value = decl.value.replace(reg, function (val) {
+        val = val.toLowerCase()
+        if (val === '1px') return val
+        return parseFloat(val) * 2 + 'px'
+      })
+    })
+  }
 })
